f79cb750799491172cd29eb8c80806981ae7c3e1
Ubaid Ullah <ubaidawan244@gmail.com>
2025-02-11 01:20:18 +0500

    pushing code updated files

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+
 const port = process.env.PORT || 3000;
 
 // Serve static files from the 'public' folder
-app.use(cors({
-    origin: 'http://localhost:3000' // Allow requests from your frontend during development
-    // For production, you would specify your actual frontend domain
-}));
+
 app.use(express.static('public'));
 
 // ... your other routes or code ...
